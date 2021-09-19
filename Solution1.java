18bdf27241497684fa4530a4053ce1cd62c6685a
chethana <62558574+Manchikatlachethana@users.noreply.github.com>
2021-09-19 16:33:37 +0530

    Solution1.java

--- Solution1.java
+++ Solution1.java
@@ -1,16 +1,15 @@
 class Solution1 {
     public int[] twoSum(int[] nums, int target) {
-        int []res = new int[nums.length];
-        for(int i=0,j=i+1;i<nums.length&&j<i;i++,j++){
-                if(nums[i]+nums[j]==target){
-                    res[i]=nums[i];
-                    res[j]=nums[j]; 
+        int [] result = new int[2]; 
+        for(int i=0; i<nums.length-1; i++){
+            for(int j=i+1; j<nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
                 }
+            }
         }
-        /*for(int i=0;i<res.length;i++)
-        {
-            System.out.print(res[i]+"");
-        }*/
-        return res;
+      return result;  
     }
 }
