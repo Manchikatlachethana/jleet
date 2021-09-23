8567256d38465ecd3241a06f44d8234745d208e3
chethana <62558574+Manchikatlachethana@users.noreply.github.com>
2021-09-23 11:22:57 +0530

    Solution11.java

---
+++ Solution11.java
@@ -0,0 +1,12 @@
+class Solution11 {
+    public int maxArea(int[] height) {
+        int maxarea = 0;
+        for(int i = 0; i<height.length; i++){
+            for(int j = i+1; j<height.length; j++){
+                maxarea = Math.max(maxarea,Math.min(height[i],height[j])*(j-i));
+                
+            }
+        }
+        return maxarea;
+    }
+}
