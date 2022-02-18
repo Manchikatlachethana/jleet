2511b4090598393d0d6de0e943ed8736e10b5f9e
chethana <62558574+Manchikatlachethana@users.noreply.github.com>
2022-02-18 15:18:39 +0530

    Create Solution203.java

---
+++ Solution203.java
@@ -0,0 +1,18 @@
+class Solution {
+  public ListNode removeElements(ListNode head, int val) {
+      if(head == null) return null;
+    ListNode res = new ListNode(-1);
+    res.next = head;
+    ListNode prev = res, curr = head;
+    while (curr != null) {
+      if (curr.val == val) {
+          prev.next = curr.next;
+      }
+      else{
+          prev = curr;
+      } 
+      curr = curr.next;
+    }
+    return res.next;
+  }
+}
