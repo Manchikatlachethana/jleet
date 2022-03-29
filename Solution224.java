1439ac7b7da8915410a509cbacc8d1fe921b0ed7
chethana <62558574+Manchikatlachethana@users.noreply.github.com>
2022-03-29 16:40:49 +0530

    calculator

---
+++ Solution224.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<>();
+       int operand = 0;
+        int result = 0;
+        int sign = 1;
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(Character.isDigit(ch)){
+                operand = 10*operand+(int)(ch-'0');
+            }else if(ch == '+'){                                 
+                result = result+sign*operand;
+                sign = 1;
+                operand = 0;
+            }else if(ch == '-'){
+                result = result+sign*operand;
+                sign = -1;
+                operand = 0;
+            }else if(ch == '('){
+                stack.push(result);
+                stack.push(sign);
+                sign = 1;
+                result = 0;
+            }else if(ch == ')'){
+                result = result+sign*operand;
+                result = result*stack.pop();
+                result = result+stack.pop();
+                operand = 0;
+            }
+        }
+        
+        return result+(sign*operand);
+    }
+}
