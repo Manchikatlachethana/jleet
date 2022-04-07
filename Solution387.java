41d2810808241bbd852ad7775c9cbcfddb798b1b
chethana <62558574+Manchikatlachethana@users.noreply.github.com>
2022-04-07 15:51:12 +0530

    String - count

---
+++ Solution387.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        Map<Character, Integer> m = new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            if(!m.containsKey(s.charAt(i))){
+                m.put(s.charAt(i),1);
+            }else{
+                m.put(s.charAt(i),m.get(s.charAt(i))+1);
+            }
+        }
+        //System.out.println(m);
+        
+        for(int i=0;i<s.length();i++){
+            if(m.get(s.charAt(i)) == 1){
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
