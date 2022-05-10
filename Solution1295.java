50b242081283e3666dd5c0016cc25001261673ee
chethana <62558574+Manchikatlachethana@users.noreply.github.com>
2022-05-10 11:09:55 +0530

    Arrays

---
+++ Solution1295.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findNumbers(int[] nums) {
+        int even = 0;
+        for(int i=0;i<nums.length;i++){
+            int len = String.valueOf(nums[i]).length();
+            if(len %2 == 0){
+                even++;
+            }
+        }
+        return even;
+    }
+}
