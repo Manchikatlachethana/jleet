209a1e729cc47ffb842e2e6fcfcafdc56114c1eb
chethana <62558574+Manchikatlachethana@users.noreply.github.com>
2022-06-07 17:36:59 +0530

    Intersection of Two Linked Lists

--- LinkedList/Solution160.java
+++ LinkedList/Solution160.java
@@ -1,3 +1,5 @@
+//Time complexity : O(N+M)
+//1
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         while(headA!=null){
@@ -11,5 +13,44 @@ public class Solution {
         return null;
     }
 }
-//Time complexity : 
-O(N+M).
+
+//2
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Set<ListNode> b = new HashSet<>();
+        while(headB!=null){
+            b.add(headB);
+            headB=headB.next;
+        }
+        while(headA!=null){
+            if(b.contains(headA)){
+                return headA;
+            }
+            headA=headA.next;
+        }
+        return null;
+    }
+}
+
+
+//3
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode t1 = headA;
+        ListNode t2 = headB;
+        while(t1!=t2){
+            if(t1==null){
+                t1=headB; 
+            }else{
+                t1=t1.next;
+            }            
+            if(t2==null){
+                t2=headA; 
+            }else{
+                t2=t2.next;
+            }
+            
+        }        
+        return t1;
+    }
+}
