73bf5dc79a89e5428a9370547125e0eb5a787607
chethana <62558574+Manchikatlachethana@users.noreply.github.com>
2022-07-02 16:06:52 +0530

    1480. Running Sum of 1d Array

---
+++ LC-75/D1/Solution1480.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] runningSum(int[] nums) {        
+        for(int i=1;i<nums.length;i++){
+            nums[i] = nums[i-1]+nums[i];
+        }
+        return nums;
+    }
+}
+/*
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int res[] = new int[nums.length];
+        res[0] = nums[0];
+        for(int i=1;i<nums.length;i++){
+            res[i] = res[i-1]+nums[i];
+        }
+        return res;
+    }
+}
+*/
